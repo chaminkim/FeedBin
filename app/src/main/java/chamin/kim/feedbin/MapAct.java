8caeaf4b76736ed46ec3e09a9ea29756ccb46acd
chaminkim <chamino.banino@gmail.com>
2023-02-15 16:31:01 +0800

    a

--- app/src/main/java/chamin/kim/feedbin/MapAct.java
+++ app/src/main/java/chamin/kim/feedbin/MapAct.java
@@ -18,6 +18,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapAct  extends AppCompatActivity
         implements OnMapReadyCallback {
 
@@ -34,12 +36,18 @@ public class MapAct  extends AppCompatActivity
         mapFragment.getMapAsync(this);
     }
 
+
+    private ArrayList<LatLng> locationArrayList;
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
+        locationArrayList = new ArrayList<>();
+
         mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        LatLng SEOUL = new LatLng(22.3193, 114.1694);
+        LatLng HK = new LatLng(22.2345, 114.1952);
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
@@ -47,6 +55,20 @@ public class MapAct  extends AppCompatActivity
         markerOptions.snippet("한국의 수도");
         mMap.addMarker(markerOptions);
 
+        locationArrayList.add(SEOUL);
+        locationArrayList.add(HK);
+
+        for (int i = 0; i < locationArrayList.size(); i++) {
+
+            // below line is use to add marker to each location of our array list.
+            mMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
+
+            // below line is use to zoom our camera on map.
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
+
+            // below line is use to move our camera to the specific location.
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
+        }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
 
